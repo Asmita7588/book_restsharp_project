1c21f655ffde9cb66c9e3f2ed718e01f6bf71804
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-03-05 16:03:45 +0530

    book data retived from json file to console

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using BookRestSharp;
+using Newtonsoft.Json;
+using RestSharp;
+
+internal class Program
+{
+    private static RestClient client;
+    private static void Main(string[] args)
+    {
+        var client = new RestClient("http://localhost:3000");
+        // Get data from json file 
+        var request = new RestRequest("Books", Method.Get);
+        var response =   client.Execute(request);
+
+        if(response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine("Data retrived successfully");
+            List<Book> bookList = JsonConvert.DeserializeObject<List<Book>>(response.Content);
+
+            if (bookList != null) {
+                foreach (var book in bookList) { 
+                    Console.WriteLine($"Id = {book.id}, title = {book.title}, author = {book.author}, avilable = {book.available}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Book not found status : {response.StatusCode}");
+            }
+
+        }
+    }
+}
